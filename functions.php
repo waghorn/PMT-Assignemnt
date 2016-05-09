e352225cef1d514a1588e564a0bb2a7d7cbe42d4
waghorn <waghorn.chris@hotmail.co.uk>
2016-05-09 15:32:44 +0100

    Function Definition
    Defined two functions and added potential ping code

--- functions.php
+++ functions.php
@@ -1,1 +1,8 @@
-TEST
+//Pings the destination
+function ping($address, $portNumber) {
+	$fp = fsockopen($address, $portNumber, $errno, $errstr, 30)
+}
+//Builds the IP address
+function buildAddress() {
+	
+}
